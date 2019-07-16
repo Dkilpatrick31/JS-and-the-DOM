37af6ebfee16eea4fd5aa18f0f7f8f038982851e
Dkilpatrick31 <dkilpatrick1990@gmail.com>
2019-07-16 01:09:16 -0500

    Finished 3-reps of this exercise. added some notes for what this objective was.

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
+// what am i doing?
+// change the color of the h1 element.
+// make the button clickable and when it's clicked it needs to change the headings color.
+// the input will make it to where the user can type in a color or hex# and it'll tell the button what color to make the heading.
+
 const myHeading = document.getElementById('myHeading');
 const myButton = document.getElementById('myButton');
 const myTextInput = document.getElementById('myTextInput');
 
 myButton.addEventListener('click', () => {
   myHeading.style.color = myTextInput.value;
-});
-
-// myButton.addEventListener('click', () => {
-//   myHeading.style.scolor = 'yellow';
-// })
+})
