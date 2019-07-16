e1449216965f8ee86e0fc8248b1b46405248d998
Dkilpatrick31 <dkilpatrick1990@gmail.com>
2019-07-16 00:45:42 -0500

    Created a color editor from a simple input element and using DOM manipulation to change the color of the myHeading id to whatever the user types into the input.

---
+++ main.js
@@ -0,0 +1,11 @@
+const myHeading = document.getElementById('myHeading');
+const myButton = document.getElementById('myButton');
+const myTextInput = document.getElementById('myTextInput');
+
+myButton.addEventListener('click', () => {
+  myHeading.style.color = myTextInput.value;
+});
+
+// myButton.addEventListener('click', () => {
+//   myHeading.style.scolor = 'yellow';
+// })
