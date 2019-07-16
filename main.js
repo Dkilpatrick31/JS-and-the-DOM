f632714988ce1fc7eec8a6b64cd86ef1761a4e57
Dkilpatrick31 <dkilpatrick1990@gmail.com>
2019-07-16 18:06:24 -0500

    Moved the input and button to be above the ul. Then messed around with .textContent and .innerHTML for a bit

--- main.js
+++ main.js
@@ -1,28 +1,7 @@
-// what am i doing?
-// change the color of the h1 element.
-// make the button clickable and when it's clicked it needs to change the headings color.
-// the input will make it to where the user can type in a color or hex# and it'll tell the button what color to make the heading.
+const input = document.querySelector('input');
+const p = document.querySelector('p.description');
+const button = document.querySelector('button');
 
-const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
-const myTextInput = document.getElementById('myTextInput');
-const myList = document.getElementsByTagName('li');
-const errorNotGreen = document.getElementsByClassName('error-not-purple');
-const odd = document.querySelectorAll('li:nth-child(odd)');
-
-// Adding and event listener, telling what to look for, then telling
-myButton.addEventListener('click', () => {
-  myHeading.style.color = myTextInput.value;
+button.addEventListener('click', () => {
+  p.innerHTML = input.value + ':';
 })
-
-for (let i = 0; i < myList.length; i += 1) {
-  myList[i].style.color = 'green';
-}
-
-for (let i = 0; i < errorNotGreen.length; i += 1) {
-  errorNotGreen[i].style.color = 'purple';
-}
-
-for (let i = 0; i < odd.length; i += 1) {
-  odd[i].style.backgroundColor = 'grey';
-}
