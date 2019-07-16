ec60e499876a731c712be68050176685d1bef99e
Dkilpatrick31 <dkilpatrick1990@gmail.com>
2019-07-16 16:50:10 -0500

    Created a few more li elements and manipulated the DOM with them using the getELementsByClassName approach in main.js

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const myHeading = document.getElementById('myHeading');
 const myButton = document.getElementById('myButton');
 const myTextInput = document.getElementById('myTextInput');
 const myList = document.getElementsByTagName('li');
+const errorNotPurple = document.getElementsByClassName('error-not-purple');
 
 // Adding and event listener, telling what to look for, then telling
 myButton.addEventListener('click', () => {
@@ -15,4 +16,8 @@ myButton.addEventListener('click', () => {
 
 for (let i = 0; i < myList.length; i += 1) {
   myList[i].style.color = 'green';
+}
+
+for (let i = 0; i < errorNotPurple.length; i += 1) {
+  errorNotPurple[i].style.color = 'red';
 }
