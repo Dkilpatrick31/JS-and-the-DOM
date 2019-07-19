81c6b25274e874efb712fc0581a88faa20cf811f
Dkilpatrick31 <dkilpatrick1990@gmail.com>
2019-07-18 21:40:30 -0500

    Created a mouseover event listener so that the listItems will all turned UPPERCASE when you hover over them with the mouse.

--- main.js
+++ main.js
@@ -7,24 +7,33 @@ const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
 
+  listDiv.addEventListener('mouseover', (event) => {
+    if (event.target.tagName == "LI") {
+      event.target.textContent = event.target.textContent.toUpperCase();
+    }
+  });
+
+  listDiv.addEventListener('mouseout', (event) => {
+    if (event.target.tagName == "LI") {
+      event.target.textContent = event.target.textContent.toLowerCase();
+    }
+  });
 // add an event listener that can show or hide the list.
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == "none") {
-    toggleList.textContent = 'Show List';
+    toggleList.textContent = 'Hide List';
     listDiv.style.display = 'block';
   } else {
-    toggleList.textContent = 'Hide List';
+    toggleList.textContent = 'Show List';
     listDiv.style.display = "none";
   }
 });
-
 
 // add an event listenever that changes what the list description says.
 descriptionButton.addEventListener('click', () => {
   descriptionP.innerHTML = descriptionInput.value + ':';
   descriptionInput.value = '';
 });
-
 
 // add an event listener that creates an li element and add the value from the input next to the add item button.
 addItemButton.addEventListener('click', () => {
@@ -35,6 +44,7 @@ addItemButton.addEventListener('click', () => {
   addItemInput.value = '';
 });
 
+// add an event listener to the removeItemButton so that it removes the last child in the list.
 removeItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.querySelector('li:last-child');
